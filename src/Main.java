967944f731193ae9f3d5925335339af3a5e80ce8
Luis <luisfelipelimadasilva@gmail.com>
2022-10-11 00:37:04 -0300

    add main App

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        ArithmeticChallenge.run();
+//        RobotChallenge.run();
+//        EvenNumbersChallenge.run();
+    }
+}
